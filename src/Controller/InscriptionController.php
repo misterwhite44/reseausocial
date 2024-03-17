349540e695644700f956315b161fa9c2b9fa2a39
louisboucard <louis44.boucard@gmail.com>
2024-03-17 23:09:09 +0100

    données sur la bdd pour inscription qui marche

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -1,18 +1,49 @@
 <?php
-// src/Controller/LuckyController.php
+
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface; // Import de la classe EntityManagerInterface
 
 class InscriptionController extends AbstractController
 {
-    #[Route('/lucky/inscription')]
-    public function number(): Response
-    {
+    private $entityManager; // Déclaration de la propriété entityManager
 
-        return $this->render('lucky/inscription.html.twig', [
-        ]);
+    // Injection du EntityManagerInterface via le constructeur
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/lucky/inscription')]
+    public function number(Request $request): Response
+    {
+        // Traitement du formulaire lors de la soumission
+        if ($request->isMethod('POST')) {
+            // Récupérer les données du formulaire
+            $pseudo = $request->request->get('username');
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+
+            // Créer une nouvelle instance de l'entité User et définir les données
+            $user = new User();
+            $user->setPseudo($pseudo);
+            $user->setEmail($email);
+            $user->setPassword($password);
+
+            // Persister l'utilisateur dans la base de données
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            // Rediriger l'utilisateur vers une autre page après l'inscription
+            return $this->redirectToRoute('app_connexion');
+        }
+
+        // Si la méthode de requête n'est pas POST, afficher simplement le formulaire
+        return $this->render('lucky/inscription.html.twig');
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,142 +3,62 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
+    #[ORM\Column(length: 30)]
+    private ?string $Pseudo = null;
 
-    #[ORM\Column]
-    private array $roles = [];
+    #[ORM\Column(length: 40)]
+    private ?string $Email = null;
 
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $username = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $nom = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $prenom = null;
+    #[ORM\Column(length: 50)]
+    private ?string $Password = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    public function getPseudo(): ?string
+    {
+        return $this->Pseudo;
+    }
+
+    public function setPseudo(string $Pseudo): static
+    {
+        $this->Pseudo = $Pseudo;
+
+        return $this;
+    }
+
     public function getEmail(): ?string
     {
-        return $this->email;
+        return $this->Email;
     }
 
-    public function setEmail(string $email): static
+    public function setEmail(string $Email): static
     {
-        $this->email = $email;
+        $this->Email = $Email;
 
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
+    public function getPassword(): ?string
     {
-        return (string) $this->email;
+        return $this->Password;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
+    public function setPassword(string $Password): static
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials(): void
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
-
-    public function setNom(string $nom): static
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
-
-    public function setPrenom(string $prenom): static
-    {
-        $this->prenom = $prenom;
+        $this->Password = $Password;
 
         return $this;
     }
